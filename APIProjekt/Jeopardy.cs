c73891cf4441d2b8c06eab0afdd888d95ba29508
Paul Chastain <paulchast1@gmail.com>
2022-11-16 13:28:34 +0100

    jeopardy wip

---
+++ APIProjekt/Jeopardy.cs
@@ -0,0 +1,8 @@
+using System.Text.Json.Serialization;
+
+public class Jeopardy
+{
+    public int id { get; set; }
+    public string answer { get; set; }
+    public string question { get; set; }
+}
--- APIProjekt/Program.cs
+++ APIProjekt/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RestSharp;
+using System.Text.Json;
+using System.Net;
+
+RestClient jeopardyClient = new("https://jservice.io");
+RestRequest request = new("/api/random");
+RestResponse response = jeopardyClient.GetAsync(request).Result;
+
+
+if (response.StatusCode == HttpStatusCode.OK)
+{
+    Jeopardy j = JsonSerializer.Deserialize<List<Jeopardy>>(response.Content).First();
+
+    Console.WriteLine(j.id);
+    Console.WriteLine(j.question);
+    Console.WriteLine(j.answer);
+}
+else
+{
+    Console.WriteLine("Error: The question does not exist");
+}
+
+
+
+Console.ReadLine();
